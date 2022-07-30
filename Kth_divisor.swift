b8a7d07d7c940423d6fef5ad57f9c94535fc920b
Jiun Na <50474006+vonreal@users.noreply.github.com>
2022-07-30 13:39:41 +0900

    Create Kth_divisor.swift
    👩🏻‍💻 파이썬 알고리즘 문제풀이 (코딩테스트 대비) - 김태원 강사님의 K번째 약수 스위프트 풀이입니다.

---
+++ Kth_divisor.swift
@@ -0,0 +1,20 @@
+func solution(_ input: String) {
+    let numbers = input.split(separator: " ").map{ Int($0)! }
+    let N = numbers[0]
+    let K = numbers[1]
+
+    var count = 0
+    for num in 1...N {
+        if (N % num == 0) {
+            count += 1
+            print("[\(count)번째]N의 약수: \(num)")
+        }
+        if (count == K) {
+            print(num)
+            return
+        }
+    }
+    print(-1)
+}
+
+solution("8 4")
